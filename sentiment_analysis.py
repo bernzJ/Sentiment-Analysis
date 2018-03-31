b4cd9dc067262ad0cea95964524cca12656e8051
benJ <ben-0_0-@hotmail.com>
2018-03-31 05:49:14 -0400

    logic change

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -20,9 +20,10 @@ async def save_database(data_json):
     db.post.insert_one(data_json)
 
 
-async def queue_analyze_data(session, results, apis, tries=0):
+async def queue_analyze_data(session, results, apis):
     for result in results:
         analyzed = [{"Error"}]
+        tries = 0
         while [data for data in analyzed if "Error" in data]:
             if "body" in result["obj"]:
                 analyzed = await analyze_data(session, result["obj"]["title"], apis)
@@ -30,6 +31,7 @@ async def queue_analyze_data(session, results, apis, tries=0):
                 analyzed = await analyze_data(session, result["obj"]["title"], apis)
             if tries >= 10:
                 break
+            tries += 1
         result["obj"] = analyzed
         await save_database(result["obj"])
 
@@ -49,9 +51,10 @@ async def scrap_comments(session, subreddit, apis):
     json_dict = json.loads(await helpers.fetch(session, "https://www.reddit.com/r/{}/comments/{}.json?".format(subreddit_dict["sub-reddit"], subreddit_dict["id"])))
 
     print("[**] scrapped comments ..")
-    saved_data = {}
+    results = []
     for data in json_dict:
         for children in data["data"]["children"]:
+            saved_data = {}
             if "title" in children["data"]:
                 saved_data["title"] = children["data"]["title"]
             saved_data["url"] = children["data"]["url"]
@@ -60,20 +63,8 @@ async def scrap_comments(session, subreddit, apis):
             saved_data["score"] = children["data"]["score"]
             saved_data["author"] = children["data"]["author"]
             saved_data["created_utc"] = children["data"]["created_utc"]
-
-            if "body" in children["data"]:
-                saved_data["body"] = children["data"]["body"]
-                saved_data["apis"] = await analyze_data(session, children["data"]["body"], apis)
-            elif "title" in children["data"]:
-                saved_data["apis"] = await analyze_data(session, children["data"]["title"], apis)
-            else:
-                return {"status": "skip"}
-
-            if [_api for _api in saved_data["apis"] if "Error" in _api]:
-                return {"status": "Error", "obj": saved_data}
-            else:
-                await save_database(saved_data)
-                return {"status": "ok"}
+            results.append(saved_data)
+    return results
 
 
 async def get_new_subs(session, after=None):
@@ -88,34 +79,28 @@ async def get_new_subs(session, after=None):
 
 
 async def queue():
-    while True:
-        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
 
-            matches = await get_new_subs(session)
-            tasks = []
-            apis = json.loads(helpers.open_save_file("./api.json", "r"))
-            last_id = matches[len(matches) - 1]
+        matches = []
+        tasks = []
+        apis = json.loads(helpers.open_save_file("./api.json", "r"))
+        last_id = matches[len(matches) - 1]
 
-            print("[**] Init, got {} matches ..".format(len(matches) - 1))
-            while True:
-                if not matches:
-                    matches = await get_new_subs(session, last_id)
-                    if matches == "Out of data !":
-                        print("[***] Out of matches, stopping ..")
-                        return
-                    last_id = matches[len(matches) - 1]
-                    print("[**] Got new matches ..")
-                match = matches.pop()
-                task = asyncio.ensure_future(
-                    scrap_comments(session, match, apis))
-                tasks.append(task)
-                tasks_results = await asyncio.gather(*tasks)
-
-                # Locked queue analyze, nothing async.
-                results = [
-                    result for result in tasks_results if result["status"] == "Error"]
-                if results:
-                    await queue_analyze_data(session, results, apis)
+        print("[**] Init, got {} matches ..".format(len(matches) - 1))
+        while True:
+            if not matches:
+                matches = await get_new_subs(session, last_id)
+                if matches == "Out of data !":
+                    print("[***] Out of matches, stopping ..")
+                    return
+                last_id = matches[len(matches) - 1]
+                print("[**] Got new matches ..")
+            match = matches.pop()
+            task = asyncio.ensure_future(
+                scrap_comments(session, match, apis))
+            tasks.append(task)
+            results = await asyncio.gather(*tasks)
+            queue_analyze_data(session, results, apis)
 
 
 def main():
