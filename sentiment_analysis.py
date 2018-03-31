5b406b0bb0fe2b1fb989adac7607233e32804944
benJ <ben-0_0-@hotmail.com>
2018-03-31 03:57:15 -0400

    bug fixes

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -18,16 +18,28 @@ async def save_database(data_json):
     client = MongoClient()
     db = client.reddit_mind
     db.post.insert_one(data_json)
-    print("[**] Saving to db: {}".format(data_json))
+
+
+async def queue_analyze_data(session, results, apis, tries=0):
+    for result in results:
+        analyzed = [{"Error"}]
+        while [data for data in analyzed if "Error" in data]:
+            if "body" in result["obj"]:
+                analyzed = await analyze_data(session, result["obj"]["title"], apis)
+            else:
+                analyzed = await analyze_data(session, result["obj"]["title"], apis)
+            if tries >= 10:
+                break
+        result["obj"] = analyzed
+        await save_database(result["obj"])
 
 
 async def analyze_data(session, data_string, apis):
     analyzed = []
-
     for api in apis:
         if "json_data" in api and "data_string" in api["json_data"]:
             api["json_data"]["data_string"] = data_string
-        analyzed.append(await helpers.fetch(session, api["url"], api["method"], api["json_data"], **api["headers"]))
+        analyzed.append({"api": api["name"], "data": await helpers.fetch(session, api["url"], api["method"], api["json_data"], **api["headers"])})
     print("[**] Analyze api data: {}".format(analyzed))
     return analyzed
 
@@ -53,11 +65,15 @@ async def scrap_comments(session, subreddit, apis):
                 saved_data["body"] = children["data"]["body"]
                 saved_data["apis"] = await analyze_data(session, children["data"]["body"], apis)
             elif "title" in children["data"]:
-                saved_data["body"] = ""
                 saved_data["apis"] = await analyze_data(session, children["data"]["title"], apis)
             else:
-                pass
-            await save_database(saved_data)
+                return {"status": "skip"}
+
+            if [_api for _api in saved_data["apis"] if "Error" in _api]:
+                return {"status": "Error", "obj": saved_data}
+            else:
+                await save_database(saved_data)
+                return {"status": "ok"}
 
 
 async def get_new_subs(session, after=None):
@@ -86,15 +102,20 @@ async def queue():
                     matches = await get_new_subs(session, last_id)
                     if matches == "Out of data !":
                         print("[***] Out of matches, stopping ..")
-                        break
+                        return
                     last_id = matches[len(matches) - 1]
                     print("[**] Got new matches ..")
                 match = matches.pop()
                 task = asyncio.ensure_future(
                     scrap_comments(session, match, apis))
                 tasks.append(task)
-            #TODO: await those 20 tasks, returns a proper fail object and redo if the analyze failed.
-            await asyncio.gather(*tasks)
+                tasks_results = await asyncio.gather(*tasks)
+
+                # Locked queue analyze, nothing async.
+                results = [
+                    result for result in tasks_results if result["status"] == "Error"]
+                if results:
+                    await queue_analyze_data(session, results, apis)
 
 
 def main():
