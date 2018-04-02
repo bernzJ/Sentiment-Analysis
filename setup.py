50e9105c3ad2256b28ec3304dab53725c35c09fc
Bernard J <ben-0_0-@hotmail.com>
2018-04-02 05:04:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="sentiment_analysis",
     version="1.0",
-    description="Update this.",
+    description="Analytic tool allowing to analyze reddit new posts/comments feed searching for specific keywords/sentences to be then sent to score apis like mashape.",
     author="Ben J",
     author_email="benje@hotmail.ca",
     packages=["sentiment_analysis"],
