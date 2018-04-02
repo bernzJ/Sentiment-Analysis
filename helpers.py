967298018c470ccad056832b213746c5b43db1fb
benJ <ben-0_0-@hotmail.com>
2018-04-02 04:41:24 -0400

    added comments

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import asyncio
 import async_timeout
 import aiohttp
@@ -13,8 +14,8 @@ logging.basicConfig(filename="app.log", level=logging.INFO)
 
 
 def open_save_file(path, mode, data_string=None, callback=None):
-    """
-    CRUD string to files, return data_string, readlines result as string or callback(params).
+    """Return string or array or [type]callback
+    RW string to files. Modes are: r w
     """
     with open(path, mode=mode, encoding="utf-8") as f:
         output = ""
@@ -32,11 +33,16 @@ def open_save_file(path, mode, data_string=None, callback=None):
 
 @DeprecationWarning
 async def bound_fetch(sem, session, url, method="GET", postdata="",  **headers):
+    """Deprecated, search aiohttp semaphore.
+    """
     async with sem:
         await fetch(session, url, method, postdata, **headers)
 
 
 async def fetch(session, url, method="GET", postdata="",  **headers):
+    """Return string
+    fetch using aiohttp, methods are: GET POST PUT
+    """
     try:
         if headers is None:
             headers = {}
@@ -53,14 +59,48 @@ async def fetch(session, url, method="GET", postdata="",  **headers):
                     return await response.text()
     except Exception as e:
         logging.error("Error: {}\nArgs: {}".format(str(e), e.args))
-        # TODO: might change this.
         return "Error"
 
 
+def log_error(error):
+    """Return None
+    Public extension of logger class, param must be string.
+    """
+    logging.error(error)
+
+
+def is_json(data):
+    """Return string or dict
+    Try to load (string)param as json, on fail return param.
+    """
+    try:
+        return json.loads(data)
+    except:
+        return data
+
+
+# return fields containing the keywords
+def has_keyword(list_post, fields, keywords):
+    """Return list
+    Search list childs for a dict containing a specific keyword.
+    """
+    results = []
+    for field in fields:
+        if field in list_post:
+            for keyword in keywords:
+                if keyword.upper() in list_post[field].upper():
+                    results.append(field)
+    return results
+
+
 def parse_reddit_url(url):
+    """Return dict or None
+    Try to parse url into reddit style.
+    """
     segments = url.split("/")
     if len(segments) is not 7:
         logging.error("Invalid sub-reddit url: {}".format(url))
+        return None
     return {
         "id": segments[4],
         "sub-reddit": segments[2],
--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,11 +3,8 @@
 import asyncio
 import async_timeout
 import aiohttp
-import random
-import aiofiles
 import json
 import re
-import logging
 import helpers
 
 from pymongo import MongoClient
@@ -21,19 +18,15 @@ async def save_database(data_json):
 
 
 async def queue_analyze_data(session, results, apis):
-    for result in results:
-        analyzed = [{"Error"}]
-        tries = 0
-        while [data for data in analyzed if "Error" in data]:
-            if "body" in result["obj"]:
-                analyzed = await analyze_data(session, result["obj"]["title"], apis)
-            else:
-                analyzed = await analyze_data(session, result["obj"]["title"], apis)
-            if tries >= 10:
-                break
-            tries += 1
-        result["obj"] = analyzed
-        await save_database(result["obj"])
+    for sub_reddit in results:
+        for post_comments in sub_reddit:
+            for _try in range(0, 10):
+                for field in post_comments["fields"]:
+                    data_api = await analyze_data(session, post_comments[field], apis)
+                    if [data for data in data_api if "Error" in data]:
+                        continue
+                    post_comments["analyzes"] = data_api
+                await save_database(post_comments)
 
 
 async def analyze_data(session, data_string, apis):
@@ -41,30 +34,43 @@ async def analyze_data(session, data_string, apis):
     for api in apis:
         if "json_data" in api and "data_string" in api["json_data"]:
             api["json_data"]["data_string"] = data_string
-        analyzed.append({"api": api["name"], "data": await helpers.fetch(session, api["url"], api["method"], api["json_data"], **api["headers"])})
+        analyzed.append({"api": api["name"], "data": helpers.is_json(await helpers.fetch(session, api["url"], api["method"], api["json_data"], **api["headers"]))})
     print("[**] Analyze api data: {}".format(analyzed))
     return analyzed
 
 
-async def scrap_comments(session, subreddit, apis):
-    subreddit_dict = helpers.parse_reddit_url(subreddit)
-    json_dict = json.loads(await helpers.fetch(session, "https://www.reddit.com/r/{}/comments/{}.json?".format(subreddit_dict["sub-reddit"], subreddit_dict["id"])))
+async def scrap_comments(session, subreddit, apis, keywords):
+    try:
+        subreddit_dict = helpers.parse_reddit_url(subreddit)
+        if not subreddit_dict:
+            return []
+        reddit_api_response = await helpers.fetch(session, "https://www.reddit.com/r/{}/comments/{}.json?".format(subreddit_dict["sub-reddit"], subreddit_dict["id"]))
+        if reddit_api_response == "Error":
+            return []
+        json_dict = json.loads(reddit_api_response)
 
-    print("[**] scrapped comments ..")
-    results = []
-    for data in json_dict:
-        for children in data["data"]["children"]:
-            saved_data = {}
-            if "title" in children["data"]:
-                saved_data["title"] = children["data"]["title"]
-            saved_data["url"] = children["data"]["url"]
-            saved_data["ups"] = children["data"]["ups"]
-            saved_data["downs"] = children["data"]["downs"]
-            saved_data["score"] = children["data"]["score"]
-            saved_data["author"] = children["data"]["author"]
-            saved_data["created_utc"] = children["data"]["created_utc"]
-            results.append(saved_data)
-    return results
+        print("[**] scrapped comments ..")
+        results = []
+        for data in json_dict:
+            for children in data["data"]["children"]:
+                fields = helpers.has_keyword(children["data"], [
+                    "title", "body"], keywords)
+                if fields:
+                    saved_data = {}
+                    for field in fields:
+                        saved_data[field] = children["data"][field]
+                    saved_data["permalink"] = children["data"]["permalink"]
+                    saved_data["ups"] = children["data"]["ups"]
+                    saved_data["downs"] = children["data"]["downs"]
+                    saved_data["score"] = children["data"]["score"]
+                    saved_data["author"] = children["data"]["author"]
+                    saved_data["created_utc"] = children["data"]["created_utc"]
+                    saved_data["fields"] = fields
+                    results.append(saved_data)
+        return results
+    except Exception as e:
+        helpers.log_error("Error: {}/nArgs: {}".format(str(e), e.args))
+        return "Error"
 
 
 async def get_new_subs(session, after=None):
@@ -83,10 +89,11 @@ async def queue():
 
         matches = []
         tasks = []
+        last_id = None
         apis = json.loads(helpers.open_save_file("./api.json", "r"))
-        last_id = matches[len(matches) - 1]
+        keywords = json.loads(helpers.open_save_file("./keyword.json", "r"))
 
-        print("[**] Init, got {} matches ..".format(len(matches) - 1))
+        print("[**] Init ..")
         while True:
             if not matches:
                 matches = await get_new_subs(session, last_id)
@@ -95,12 +102,14 @@ async def queue():
                     return
                 last_id = matches[len(matches) - 1]
                 print("[**] Got new matches ..")
-            match = matches.pop()
-            task = asyncio.ensure_future(
-                scrap_comments(session, match, apis))
-            tasks.append(task)
-            results = await asyncio.gather(*tasks)
-            queue_analyze_data(session, results, apis)
+            while matches:
+                match = matches.pop()
+                task = asyncio.ensure_future(
+                    scrap_comments(session, match, apis, keywords))
+                tasks.append(task)
+            results = [result for result in await asyncio.gather(*tasks) if result and result is not "Error"]
+            if results:
+                await queue_analyze_data(session, results, apis)
 
 
 def main():
